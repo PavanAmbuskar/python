82e1e31efaa39d78f92e8d18471343218387f59d
PavanAmbuskar <166595571+PavanAmbuskar@users.noreply.github.com>
2024-07-26 11:35:17 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,10 @@
+length = 30 #integer
+width = 20
+name = 'paul' #string
+price = 54.5  #float
+mybol = True # boolian value
+print (f'area of rectangle is: {width*length}')
+print (f'my name is: {name}')
+print (type (name))
+result= (2*(3+4)**2//5-6)
+print (result)
